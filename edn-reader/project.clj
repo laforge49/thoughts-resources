499dfd02881551f923a4fca0e7cd25472f5d5c41
Bill La Forge <laforge49@gmail.com>
2017-03-27 14:20:42 -0400

    edn-reader

---
+++ edn-reader/project.clj
@@ -0,0 +1,15 @@
+(defproject
+  boot-project
+  "0.0.0-SNAPSHOT"
+  :dependencies
+  [[adzerk/boot-cljs "1.7.228-2" :scope "test"]
+   [adzerk/boot-cljs-repl "0.3.3" :scope "test"]
+   [adzerk/boot-reload "0.4.13" :scope "test"]
+   [pandeiro/boot-http "0.7.6" :scope "test"]
+   [com.cemerick/piggieback "0.2.1" :scope "test"]
+   [org.clojure/tools.nrepl "0.2.12" :scope "test"]
+   [weasel "0.7.0" :scope "test"]
+   [org.clojure/clojurescript "1.9.293"]
+   [reagent "0.6.0"]]
+  :source-paths
+  ["src/cljs" "resources"])
